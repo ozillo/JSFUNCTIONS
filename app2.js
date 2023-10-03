b809fc1481015fd7c44613853cac77a6422e0b23
ozillo <marcmateo25@gmail.com>
2023-10-03 18:57:05 +0200

    It#2

--- app2.js
+++ app2.js
@@ -3,11 +3,12 @@
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 function findLongestWord(param) {
  let longestWord =""
- param.forEach(hero, index => {
-    hero.length >longestWord.length
-    ?(longestWord=hero)
-    :(longestWord=longestWord)
+ param.forEach(hero => {
+    hero.length>longestWord.length
+    ? longestWord=hero
+    : longestWord=longestWord
     
  });
- console.log(longestWord);
+ return longestWord;
 };
+console.log(findLongestWord(avengers));
