263b92df4eca60db1aa684256054cdaf9c829cff
ozillo <marcmateo25@gmail.com>
2023-10-03 19:04:19 +0200

    It#1

--- app1.js
+++ app1.js
@@ -14,16 +14,3 @@ function sum(numberOne , numberTwo) {
 const result= sum(3,6);
 console.log(result);
 
-//Iteración #2: Buscar la palabra mas larga
-
-const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
- let longestWord =""
- param.forEach(hero, index => {
-    hero.length >longestWord.length
-    ?(longestWord=hero)
-    :(longestWord=longestWord)
-    
- });
- console.log(longestWord);
-};
