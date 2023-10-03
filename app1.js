f6a99c7022cad6e0395e87073a54fb7ae8c32fc4
ozillo <marcmateo25@gmail.com>
2023-10-03 18:28:31 +0200

    It#1

---
+++ app1.js
@@ -0,0 +1,15 @@
+//Iteración #1: Buscar el máximo
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo){
+        console.log("numberOne")
+        return numberOne
+    }
+    else {
+        console.log("numberTwo")
+        return numberTwo
+    }
+   
+  };
+const result= sum(3,6);
+console.log(result);
