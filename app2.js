1df3997f52ac5b05ddd21b441b1387dd9da25cb1
ozillo <marcmateo25@gmail.com>
2023-10-03 18:40:42 +0200

    It#2

--- app2.js
+++ app2.js
@@ -1,19 +1,3 @@
-//Iteración #1: Buscar el máximo
-
-function sum(numberOne , numberTwo) {
-    if (numberOne > numberTwo){
-        console.log("numberOne")
-        return numberOne
-    }
-    else {
-        console.log("numberTwo")
-        return numberTwo
-    }
-   
-  };
-const result= sum(3,6);
-console.log(result);
-
 //Iteración #2: Buscar la palabra mas larga
 
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
